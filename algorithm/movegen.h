c6ac5fe31859954c9d786277fd583c04dafa118a
Khai Nguyen <khainv2@gmail.com>
2024-08-30 17:35:16 +0700

    260 MNode/s normal pc

--- algorithm/movegen.h
+++ algorithm/movegen.h
@@ -13,21 +13,31 @@ public:
     inline BB getPinMaskDiagonal() const noexcept;
     inline BB getPinMaskCross() const noexcept;
 
-    inline int genMoveList(const Board &board, Move *moveList) noexcept;
+    int genMoveList(const Board &board, Move *moveList) noexcept;
 
-
-    template <Color color>
+    template <Color color, bool isJustCount>
     inline Move *genMoveList(const Board &board, Move *moveList) noexcept;
 
     template <Color color>
     inline Move *getMoveListForPawn(const Board &board, Move *moveList) noexcept;
 
-    template <Color color, PieceType piece>
-    inline Move *getMoveListForPiece(const Board &board, Move *moveList) noexcept;
-
+    template <Color color>
+    inline Move *getMoveListForKnight(const Board &board, Move *moveList) noexcept;
 
     template <Color color>
-    inline Move *getMoveListWhenDoubleCheck(const Board &board, Move *moveList) noexcept;
+    inline Move *getMoveListForBishopQueen(const Board &board, Move *moveList) noexcept;
+
+    template <Color color>
+    inline Move *getMoveListForRookQueen(const Board &board, Move *moveList) noexcept;
+
+    template <Color color>
+    inline Move *getMoveListForKing(const Board &board, Move *moveList) noexcept;
+
+    template <Color color, CastlingRights right>
+    inline Move *getCastlingMoveList(const Board &board, Move *moveList) noexcept;
+
+    template <Color color>
+    inline Move *getMoveListWhenDoubleCheck(Move *moveList) noexcept;
 
     template<Color mine>
     inline BB getKingBan(const Board &board) noexcept;
@@ -44,6 +54,7 @@ private:
     BB _enemyRookQueens;
     BB _enemyBishopQueens;
 
+    BB kingBan;
     BB checkMask;
     BB pinMaskDiagonal;
     BB pinMaskCross;
